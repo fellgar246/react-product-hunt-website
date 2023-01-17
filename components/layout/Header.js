03a822b2ad7f1e24543a401c0d9cc335b494b06e
fellgar246 <fellgar246@gmail.com>
2023-01-17 14:55:06 -0600

    first commit

---
+++ components/layout/Header.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import React from 'react';
+import Buscar from '../ui/Buscar';
+import Navegacion from './Navegacion';
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <div>
+                <p>P</p>
+
+                <Buscar />
+
+                <Navegacion />
+
+            </div>
+            <div>
+                <p>Hola: Juan</p> 
+
+                <button type='button'>Cerrar Sesión</button> 
+
+                <Link href="/">Login</Link>  
+                <Link href="/">Crear Cuenta</Link>  
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
